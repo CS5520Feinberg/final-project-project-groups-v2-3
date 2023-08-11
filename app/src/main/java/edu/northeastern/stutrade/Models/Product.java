7096821d3bef4086775cb3b0e6f828bbab33518c
venkatsaivulli <vulli.v@northeastern.edu>
2023-08-10 21:32:08 -0400

    Adding seller detaild into the product model

--- app/src/main/java/edu/northeastern/stutrade/Models/Product.java
+++ app/src/main/java/edu/northeastern/stutrade/Models/Product.java
@@ -1,5 +1,4 @@
 package edu.northeastern.stutrade.Models;
-
 import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -9,96 +8,106 @@ import java.util.Locale;
 
 public class Product implements Serializable {
     private String productPrice;
+    private String sellerId;
     private String productName;
     private String datePosted;
-
-    private String location;
     private String productDescription;
+    private String sellerName;
+    private String imageUrl;
 
-    public String getSellerName() {
-        return sellerName;
+    public Product() {
+    }
+
+    public Product(String productName, String description, String price, String imageUrl, String sellerName, String sellerId, String datePosted) {
+        this.productName = productName;
+        this.productDescription = description;
+        this.productPrice = price;
+        this.imageUrl = imageUrl;
+        this.sellerName = sellerName;
+        this.datePosted = datePosted;
+        this.sellerId = sellerId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setSellerId(String userId) {
+        this.sellerId = userId;
     }
 
     public void setSellerName(String sellerName) {
         this.sellerName = sellerName;
     }
 
-    private String sellerName;
-    private List<String> imageUrls; // List of image URLs
+    public String getSellerName() {
+        return sellerName;
+    }
 
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
-    public Product(){}
-    public Product(String productPrice, String productName, String datePosted, String location, String productDescription) {
-        this.productPrice = productPrice;
-        this.productName = productName;
-        this.datePosted = datePosted;
-        this.location = location;
-        this.productDescription = productDescription;
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
     public String getProductPrice() {
         return productPrice;
     }
 
+    public String getSellerId() {
+        return sellerId;
+    }
+
     public void setProductPrice(String productPrice) {
         this.productPrice = productPrice;
     }
 
-    public String getProductName() {
-        return productName;
-    }
 
-    public void setName(String productName) {
-        this.productName = productName;
-    }
 
-    public String getDatePosted() {
-        return datePosted;
-    }
 
-    public void setDatePosted(String datePosted) {
-        this.datePosted = datePosted;
-    }
 
-    public String getLocation() {
-        return location;
-    }
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
+public void setDatePosted(String datePosted) {
 
-    public String getProductDescription() {
-        return productDescription;
-    }
-
-    public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
-    }
-
-    public Double getPriceAsDouble() {
-        try {
-            return Double.parseDouble(productPrice);
-        } catch (NumberFormatException e) {
-            return 0.0;
+        public String getProductName() {
+            return productName;
         }
-    }
-
-    public Date getDatePostedAsDate() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-        try {
-            return sdf.parse(datePosted);
-        } catch (ParseException e) {
-            return new Date();
+        public void setName(String productName) {
+            this.productName = productName;
         }
-    }
+        public String getDatePosted() {
+            return datePosted;
+        }
+        public void setDatePosted(String datePosted) {
+            this.datePosted = datePosted;
+        }
+
+        public String getProductDescription() {
+            return productDescription;
+        }
 
 
-    public List<String> getImageUrls() {
-        return imageUrls;
-    }
+        public Date getDatePostedAsDate() {
 
-    public void setImageUrls(List<String> imageUrls) {
-        this.imageUrls = imageUrls;
-    }
-}
+            public void setProductDescription(String productDescription) {
+                this.productDescription = productDescription;
+            }
+            public Double getPriceAsDouble() {
+                try {
+                    return Double.parseDouble(productPrice);
+                } catch (NumberFormatException e) {
+                    return 0.0;
+                }
+            }
+            public Date getDatePostedAsDate() {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+                try {
+                    return sdf.parse(datePosted);
+                } catch (ParseException e) {
+                    return new Date();
+                }
+            }
+
+        }
