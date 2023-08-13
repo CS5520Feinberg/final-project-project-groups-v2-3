1664638422dd48d5779f34effa04fb8f51ce9688
venkatsaivulli <vulli.v@northeastern.edu>
2023-08-13 17:25:38 -0400

    fixed the camera access bug in the sellers fragment

--- app/src/main/java/edu/northeastern/stutrade/SellerFragment.java
+++ app/src/main/java/edu/northeastern/stutrade/SellerFragment.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -19,6 +21,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -44,14 +47,15 @@ import java.util.Locale;
 
 import edu.northeastern.stutrade.Models.Product;
 import edu.northeastern.stutrade.Models.ProductViewModel;
-
 import android.Manifest;
+import android.content.pm.PackageManager;
 
 
 public class SellerFragment extends Fragment {
 
     private Button galleryButton, uploadButton;
     private static final int REQUEST_CAMERA_PERMISSION = 102; // Define your own request code
+    private TextView emptyTextView;
 
     private static final int REQUEST_IMAGE_PICK = 100;
     private static final int REQUEST_IMAGE_CAPTURE = 101;
@@ -108,7 +112,7 @@ public class SellerFragment extends Fragment {
         }
     }
 
-    private void showImageSourceDialog() {
+    private void showImageSourceDialog(){
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setTitle("Select Image Source");
         // Permission is already granted, proceed with camera operations
@@ -121,7 +125,6 @@ public class SellerFragment extends Fragment {
         });
         builder.show();
     }
-
     private void selectImagesFromGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
@@ -149,7 +152,7 @@ public class SellerFragment extends Fragment {
                 showImageSourceDialog();
             } else {
                 // Permission denied, show a message or handle accordingly
-                Toast.makeText(getContext(), "Camera access denied. Please enable permissions", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(),"Camera access denied. Please enable permissions",Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -176,36 +179,118 @@ public class SellerFragment extends Fragment {
                 // Create a new ImageView for the selected image and add it to the container
                 displaySelectedImage(imageUri);
             }
-        } else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK && currentImageUri != null) {
-            // Display the captured image directly without resizing
-            displaySelectedImage(currentImageUri);
-            selectedImageUris.add(currentImageUri);
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK && data != null) {
+            //Bitmap capturedImage = (Bitmap) data.getExtras().get("data");
+            Bitmap capturedImage = getBitmapFromUri(currentImageUri);
+            if(capturedImage!=null) {
+                Uri imageUri = getImageUri(getContext(), capturedImage);
+            // Create a new ImageView for the captured image and add it to the container
+            displaySelectedImage(imageUri);
+            selectedImageUris.add(imageUri);}
+            else{
+                Toast.makeText(getContext(),"Failed to process captured Image", Toast.LENGTH_SHORT).show();
+            }
         }
     }
-
-    private void displaySelectedImage(Uri imageUri) {
-        // Load the original image without resizing
+    private Bitmap getBitmapFromUri(Uri uri) {
         try {
-            InputStream inputStream = getContext().getContentResolver().openInputStream(imageUri);
-            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            InputStream inputStream = requireContext().getContentResolver().openInputStream(uri);
 
-            // Create a new ImageView for the selected image
-            ImageView imageView = new ImageView(getContext());
-            imageView.setLayoutParams(new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-            imageView.setImageBitmap(bitmap);
+            // Decode the image while considering orientation
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = false;
+            options.inSampleSize = 1;  // You might want to adjust this value for memory efficiency
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream, null, options);
 
-            // Add the ImageView to the imageContainer LinearLayout
-            imageContainer.addView(imageView);
+            // Rotate the bitmap based on its orientation
+            int orientation = getOrientation(uri);
+            if (orientation != 0) {
+                Matrix matrix = new Matrix();
+                matrix.postRotate(orientation);
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            }
 
-            // Close the input stream
-            inputStream.close();
+            return bitmap;
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
     }
+
+    private int getOrientation(Uri uri) {
+        try {
+            InputStream inputStream = requireContext().getContentResolver().openInputStream(uri);
+            ExifInterface exif = new ExifInterface(inputStream);
+
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            int rotationDegrees = 0;
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotationDegrees = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotationDegrees = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotationDegrees = 270;
+                    break;
+            }
+
+            return rotationDegrees;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+
+
+    private void displaySelectedImage(Uri imageUri) {
+        // Load and resize the image
+        Bitmap thumbnail = getResizedBitmap(imageUri, 350, 350);
+
+        // Create a new ImageView for the selected image
+        ImageView imageView = new ImageView(getContext());
+        imageView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        imageView.setImageBitmap(thumbnail);
+
+        // Add the ImageView to the imageContainer LinearLayout
+        imageContainer.addView(imageView);
+    }
+
+    private Bitmap getResizedBitmap(Uri imageUri, int newWidth, int newHeight) {
+        try {
+            InputStream inputStream = getContext().getContentResolver().openInputStream(imageUri);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(inputStream, null, options);
+
+            inputStream.close();
+
+            inputStream = getContext().getContentResolver().openInputStream(imageUri);
+
+            int originalWidth = options.outWidth;
+            int originalHeight = options.outHeight;
+
+            int scaleFactor = Math.min(originalWidth / newWidth, originalHeight / newHeight);
+
+            options.inJustDecodeBounds = false;
+            options.inSampleSize = scaleFactor;
+
+            Bitmap resizedBitmap = BitmapFactory.decodeStream(inputStream, null, options);
+            inputStream.close();
+
+            return resizedBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
 
     private Uri getImageUri(Context context, Bitmap imageBitmap) {
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
@@ -274,8 +359,6 @@ public class SellerFragment extends Fragment {
                     Toast.makeText(getContext(), "Product uploaded successfully", Toast.LENGTH_SHORT).show();
                     progressDialog.dismiss();
                     // Clear the fields
-                    imageContainer.removeAllViews();
-                    selectedImageUris.clear();
                     productNameEditText.setText("");
                     productDescriptionEditText.setText("");
                     productPriceEditText.setText("");
